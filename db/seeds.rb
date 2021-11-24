b619a5c93657899f5f70cfe2c3869a40363c9c5b
Jean Youn Lee <jean.youn.lee@gmail.com>
2021-11-23 18:05:00 -0800

    Run db:reset and reset all db

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,54 +9,56 @@
 require 'csv'
 require 'byebug'
 
-# DO NOT DELETE: DEMO USER
-# User.create({email: "demo@email.com", password:"111111"})
+##### DO NOT DELETE: DEMO USER #####
+User.create({email: "demo@email.com", password:"111111"})
 
 
-# PROVIDERS SEED
-# PROVIDER_DEV_CSV = 'providers_table-LA.csv'
-# PROVIDER_PROD_CSV = 'providers_table.csv'
+##### PROVIDERS SEED #####
+PROVIDER_DEV_CSV = 'providers_table-LA.csv'
+PROVIDER_PROD_CSV = 'providers_table.csv'
 
-# CSV.foreach(Rails.root.join("lib/seed_csv/#{PROVIDER_DEV_CSV}"), headers: true) do |row|
+CSV.foreach(Rails.root.join("lib/seed_csv/#{PROVIDER_DEV_CSV}"), headers: true) do |row|
 
-#   Provider.create( {
-#     npi: row["npi"], 
-#     name: row["name"],
-#     provider_type: row["provider_type"], 
-#     address_1: row["address_1"],
-#     address_2: row["address_2"],
-#     city: row["city"],
-#     state: row["state"],
-#     zip_code: row["zip_code"],
-#     latitude: row["latitude"].to_f,
-#     longitude: row["longitude"].to_f,
-#   } ) 
-# end
+  Provider.create( {
+    npi: row["npi"], 
+    name: row["name"],
+    provider_type: row["provider_type"], 
+    address_1: row["address_1"],
+    address_2: row["address_2"],
+    city: row["city"],
+    state: row["state"],
+    zip_code: row["zip_code"],
+    latitude: row["latitude"].to_f,
+    longitude: row["longitude"].to_f,
+  } ) 
+end
 
-# INSURANCES SEED
-# INSURANCE_CSV = 'insurances_table.csv'
+##### INSURANCES SEED #####
+INSURANCE_CSV = 'insurances_table.csv'
 
-# CSV.foreach(Rails.root.join("lib/seed_csv/#{INSURANCE_CSV}"), headers: true) do |row|
+CSV.foreach(Rails.root.join("lib/seed_csv/#{INSURANCE_CSV}"), headers: true) do |row|
 
-#   Insurance.create( {
-#     hios_id: row["hios_id"], 
-#     carrier: row["carrier"],
-#     plan_id: row["plan_id"], 
-#     plan: row["plan"],
-#   } ) 
-# end
+  Insurance.create( {
+    hios_id: row["hios_id"], 
+    carrier: row["carrier"],
+    plan_id: row["plan_id"], 
+    plan: row["plan"],
+  } ) 
+end
 
-# SPECIALTIES SEED
-# SPECIALTY_CSV = 'specialties_table.csv'
+##### SPECIALTIES SEED #####
+SPECIALTY_CSV = 'specialties_table.csv'
 
-# CSV.foreach(Rails.root.join("lib/seed_csv/#{SPECIALTY_CSV}"), headers: true) do |row|
+CSV.foreach(Rails.root.join("lib/seed_csv/#{SPECIALTY_CSV}"), headers: true) do |row|
 
-#   Specialty.create( {
-#     specialty_code: row["specialty_code"], 
-#     specialty_name: row["specialty_name"],
-#   } ) 
+  Specialty.create( {
+    specialty_code: row["specialty_code"], 
+    specialty_name: row["specialty_name"],
+  } ) 
 
-# end
+end
+
+##### PROVIDERSPECIALTIES SEED #####
 
 PROVIDER_SPECIALTIES_DEV = "npi_specialties_join-LA.csv"
 PROVIDER_SPECIALTIES_PROD = "npi_specialties_join.csv"
