b55a2ebc908895b6c8f575804ecb084799b4510e
Jean Youn Lee <jean.youn.lee@gmail.com>
2021-12-02 15:23:26 -0800

    Prepare to push appointment feature to heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,88 +11,88 @@ require 'csv'
 
 ##### DO NOT DELETE: DEMO USER #####
 
-# User.create({
-#   email: "demo@email.com", 
-#   password:"111111",
-#   first_name: "John",
-#   last_name: "Doe",
-#   date_of_birth: "2000-01-01",
-#   plan_id: "40513CA0380001"
-# })
+User.create({
+  email: "demo@email.com", 
+  password:"111111",
+  first_name: "John",
+  last_name: "Doe",
+  date_of_birth: "2000-01-01",
+  plan_id: "40513CA0380001"
+})
 
 
 ##### PROVIDERS SEED #####
 
-# PROVIDER_DEV_CSV = 'providers_table-LA.csv'
-# PROVIDER_PROD_CSV = 'providers_table.csv'
+PROVIDER_DEV_CSV = 'providers_table-LA.csv'
+PROVIDER_PROD_CSV = 'providers_table.csv'
 
-# CSV.foreach(Rails.root.join("lib/seed_csv/#{PROVIDER_DEV_CSV}"), headers: true) do |row|
+CSV.foreach(Rails.root.join("lib/seed_csv/#{PROVIDER_DEV_CSV}"), headers: true) do |row|
 
-#   Provider.create( {
-#     npi: row["npi"], 
-#     name: row["name"],
-#     provider_type: row["provider_type"], 
-#     address_1: row["address_1"],
-#     address_2: row["address_2"],
-#     city: row["city"],
-#     state: row["state"],
-#     zip_code: row["zip_code"],
-#     latitude: row["latitude"].to_f,
-#     longitude: row["longitude"].to_f,
-#   } ) 
-# end
+  Provider.create( {
+    npi: row["npi"], 
+    name: row["name"],
+    provider_type: row["provider_type"], 
+    address_1: row["address_1"],
+    address_2: row["address_2"],
+    city: row["city"],
+    state: row["state"],
+    zip_code: row["zip_code"],
+    latitude: row["latitude"].to_f,
+    longitude: row["longitude"].to_f,
+  } ) 
+end
 
 
 ##### INSURANCES SEED #####
 
-# INSURANCE_CSV = 'insurances_table.csv'
+INSURANCE_CSV = 'insurances_table.csv'
 
-# CSV.foreach(Rails.root.join("lib/seed_csv/#{INSURANCE_CSV}"), headers: true) do |row|
+CSV.foreach(Rails.root.join("lib/seed_csv/#{INSURANCE_CSV}"), headers: true) do |row|
 
-#   Insurance.create( {
-#     hios_id: row["hios_id"], 
-#     carrier: row["carrier"],
-#     plan_id: row["plan_id"], 
-#     plan: row["plan"],
-#   } ) 
-# end
+  Insurance.create( {
+    hios_id: row["hios_id"], 
+    carrier: row["carrier"],
+    plan_id: row["plan_id"], 
+    plan: row["plan"],
+  } ) 
+end
 
 # Add Self-pay as a Insurance 
 
-# Insurance.create({
-#   hios_id: "00000", 
-#   carrier:"Self-pay", 
-#   plan_id:"00000ZZ0000000", 
-#   plan:"Cash"
-# })
+Insurance.create({
+  hios_id: "00000", 
+  carrier:"Self-pay", 
+  plan_id:"00000ZZ0000000", 
+  plan:"Cash"
+})
 
 ##### SPECIALTIES SEED #####
 
-# SPECIALTY_CSV = 'specialties_table.csv'
+SPECIALTY_CSV = 'specialties_table.csv'
 
-# CSV.foreach(Rails.root.join("lib/seed_csv/#{SPECIALTY_CSV}"), headers: true) do |row|
+CSV.foreach(Rails.root.join("lib/seed_csv/#{SPECIALTY_CSV}"), headers: true) do |row|
 
-#   Specialty.create( {
-#     specialty_code: row["specialty_code"], 
-#     specialty_name: row["specialty_name"],
-#   } ) 
+  Specialty.create( {
+    specialty_code: row["specialty_code"], 
+    specialty_name: row["specialty_name"],
+  } ) 
 
-# end
+end
 
 
 ##### PROVIDERSPECIALTIES SEED #####
 
-# PROVIDER_SPECIALTIES_DEV = "npi_specialties_join-LA.csv"
-# PROVIDER_SPECIALTIES_PROD = "npi_specialties_join.csv"
+PROVIDER_SPECIALTIES_DEV = "npi_specialties_join-LA.csv"
+PROVIDER_SPECIALTIES_PROD = "npi_specialties_join.csv"
 
-# CSV.foreach(Rails.root.join("lib/seed_csv/#{PROVIDER_SPECIALTIES_DEV}"), headers: true) do |row|
+CSV.foreach(Rails.root.join("lib/seed_csv/#{PROVIDER_SPECIALTIES_DEV}"), headers: true) do |row|
 
-#   ProviderSpecialty.create( {
-#     npi: row["npi"], 
-#     specialty_code: row["specialty_code"],
-#   } ) 
+  ProviderSpecialty.create( {
+    npi: row["npi"], 
+    specialty_code: row["specialty_code"],
+  } ) 
 
-# end
+end
 
 
 
@@ -100,8 +100,8 @@ require 'csv'
 
 #SEED CODE
 
-all_npi = Provider.select(:npi).distinct.pluck(:npi)
 # all_npi = Provider.select(:npi).distinct.limit(4).pluck(:npi)
+all_npi = Provider.select(:npi).distinct.pluck(:npi)
 carrier_hios_ids = Insurance.select(:hios_id).distinct.pluck(:hios_id)
 
 all_npi.each do |npi|
@@ -109,8 +109,4 @@ all_npi.each do |npi|
   all_rand_plan_ids = []
   rand_carrier_hios_ids.each {|hios| all_rand_plan_ids +=  Insurance.where(hios_id: hios).pluck(:plan_id)}
   all_rand_plan_ids.each {|plan_id| ProviderInsurance.create({npi: npi, plan_id: plan_id})}
-  # all_rand_plan_ids.each do |plan_id| 
-  #   prov_ins = ProviderInsurance.new({npi: npi, plan_id: plan_id})
-  #   debugger
-  # end
 end
