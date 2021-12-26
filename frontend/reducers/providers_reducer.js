7265aed129ebb6b7fb882d4eedee125b170e8754
Jean Youn Lee <69123522+jyl625@users.noreply.github.com>
2021-12-25 19:31:32 -0800

    Remove unnecessary action and add more prodiver state

--- frontend/reducers/providers_reducer.js
+++ frontend/reducers/providers_reducer.js
@@ -1,7 +1,7 @@
 import { RECEIVE_PROVIDER, RECEIVE_PROVIDERS } from '../actions/provider_actions'
 import { RECEIVE_CURRENT_USER } from "../actions/session_actions";
 
-// const providersReducer = (oldState = {all: {}. single: {}}, action) => {
+// const providersReducer = (oldState = {all: {}, single: {}, user: {}}, action) => {
 const providersReducer = (oldState = {}, action) => {
   Object.freeze(oldState);
 
@@ -11,7 +11,6 @@ const providersReducer = (oldState = {}, action) => {
     case RECEIVE_PROVIDER:
       newState[action.payload.provider.id] = action.payload.provider
       return newState
-    // case RECEIVE_CURRENT_USER_DETAILS:
     case RECEIVE_CURRENT_USER:
       Object.assign(newState, action.payload.providers);
       return newState;
